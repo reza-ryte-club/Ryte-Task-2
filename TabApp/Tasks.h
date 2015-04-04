8506a324ecc585bf6dce2d0c36422e237e63cd7f
Reza <mrezaurrahman@live.com>
2015-04-05 04:26:35 +0600

    Data model subclasses have been generated

---
+++ TabApp/Tasks.h
@@ -0,0 +1,20 @@
+//
+//  Tasks.h
+//  TabApp
+//
+//  Created by RahmanMd. Rezaur on 4/5/15.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Tasks : NSManagedObject
+
+@property (nonatomic, retain) NSString * course;
+@property (nonatomic, retain) NSDate * due_date;
+@property (nonatomic, retain) NSString * teacher;
+@property (nonatomic, retain) NSString * topic;
+
+@end
---
+++ TabApp/Tasks.m
@@ -0,0 +1,19 @@
+//
+//  Tasks.m
+//  TabApp
+//
+//  Created by RahmanMd. Rezaur on 4/5/15.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+#import "Tasks.h"
+
+
+@implementation Tasks
+
+@dynamic course;
+@dynamic due_date;
+@dynamic teacher;
+@dynamic topic;
+
+@end
